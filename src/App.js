b38082ea0ee5877e037f3ec6e89679b12dece9ce
Will Warren <willwarrenmail@gmail.com>
2019-03-11 14:45:07 -0400

    got buttons and api info displaying and functionin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+
+import Dog from './components/dog'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <Dog />
+      </>
+    )
   }
 }
 
---
+++ src/components/dog.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+export default function Dog() {
+  const [dog, setDog] = useState('')
+  const [classHidden, setClassHidden] = useState('hide')
+  useEffect(() => {
+    axios.get('https://dog.ceo/api/breeds/image/random').then(resp => {
+      console.log(resp.data.message)
+      setDog(resp.data.message)
+    })
+  }, [])
+
+  const revealImage = () => {
+    setClassHidden('unhide')
+  }
+
+  return (
+    <>
+      <main>
+        <h1>Click a button to see a dog!</h1>
+        <h2>You can only click once,</h2>
+        <h3>but there are no wrong answers :)</h3>
+        <section>
+          <figure
+            className={classHidden}
+            style={{
+              width: '100%',
+              height: '30rem',
+              backgroundImage: `url('${dog}')`,
+              backgroundImageSize: 'cover',
+              backgroundRepeat: 'no-repeat'
+            }}
+          />
+        </section>
+        <nav>
+          <button onClick={revealImage}>Option 1</button>
+          <button onClick={revealImage}>Option 2</button>
+          <button onClick={revealImage}>Option 3</button>
+        </nav>
+      </main>
+    </>
+  )
+}
